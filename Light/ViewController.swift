873c4eb096d23564c9d26b1c6824db00009ccaad
Raisa Frolova <fraisa@macbook-pro-raisa.local>
2018-12-02 20:02:03 +0400

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Raisa Frolova on 02.12.2018.
+//  Copyright © 2018 Raisa Frolova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
